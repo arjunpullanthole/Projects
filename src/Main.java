d40a82af828c35213183224b1aecffdb8d14f447
Arjun Pullanthole <arjunpullanthole@gmail.com>
2023-05-31 12:36:36 -0500

    Assignment day 2: Student class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.printf("Hello and welcome!\n");
+        Student st = new Student();
+        System.out.printf("Enter a name for the student\n");
+        Scanner Obj = new Scanner(System.in);
+        String name = Obj.nextLine();
+        //String name = System.console().readLine();
+        st.setName(name);
+        System.out.printf("The Student name is :%s\n", st.getName());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student
+{
+    public String Name;
+    public void setName(String name)
+    {
+        this.Name = name;
+    }
+    public String getName()
+    {
+        return this.Name;
+    }
+
+}
