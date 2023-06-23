924dacb75815b4c7f186c04c4332cb62d9727537
Arjun Pullanthole <arjunpullanthole@gmail.com>
2023-06-23 11:32:59 -0500

    Hashset Concepts

--- src/week4/Main.java
+++ src/week4/Main.java
@@ -6,7 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        taskMap();
+        taskSet();
+    }
+    public static void taskSet()
+    {
+        System.out.println("\n============:Task1:===============");
+        HashSet<String> hs = new HashSet<>();
+        List<String> ls = new ArrayList<>();
+        ls.add("John");
+        ls.add("Alice");
+        ls.add("Michael");
+        ls.add("Emily");
+        ls.add("David");
+        ls.add("Jason");
+        ls.add("Mike");
+        ls.add("Henry");
+        ls.add("Smith");
+        ls.add("Johnson");
+        hs.addAll(ls);
+        hs.forEach(s->System.out.printf("%s ",s));      //Foreach
+        System.out.println();
+        hs.stream().forEach(s->System.out.printf("%s ",s));     //Streams foreach
+        System.out.println();
+        Iterator<String> itr = hs.iterator();
+        while(itr.hasNext())
+            System.out.printf("%s ",itr.next());                //Iterator
+
+        System.out.println("\n============:Task2:===============");
+        String st = "aaaabbbbccccddddeeeefff";
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> h = new HashSet<>();
+        System.out.printf("String : %s\n",st);
+        for(int i =0;i<st.length();i++)
+            h.add(st.charAt(i));
+        h.forEach(c -> sb.append(c) );
+        System.out.printf("Unique String : %s",sb.toString());
     }
     public static void taskMap() {
         System.out.println("\n============:Task1:===============");
