c175708f1b1de124f73752dfba12e737aad5e86f
Arjun Pullanthole <arjunpullanthole@gmail.com>
2023-07-06 22:33:59 -0500

    Rest Concepts

---
+++ src/main/java/com/example/demo/DataService.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DataService {
+    private static final List<String> lst = new ArrayList<String>();
+    public List<String> fetch()
+    {
+        return lst;
+    }
+    public void add(String st)
+    {
+        lst.add(st);
+    }
+    public void update(String prev, String st)
+    {
+        Collections.replaceAll(lst,prev,st);
+    }
+    public void delete(String st)
+    {
+        lst.removeIf(s->s.equals(st));
+    }
+}
